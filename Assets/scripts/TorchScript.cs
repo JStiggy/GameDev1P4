ff4ea04669e928f59e8213f48f5c6ced93aa844c
Ferrohound <ferrohound@gmail.com>
2016-12-02 02:11:25 -0500

    Light intensity script for torches
    Give the torch flames a slight pulsate-y glow, to be tweaked slightly

---
+++ Assets/scripts/TorchScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchScript : MonoBehaviour {
+	
+	public float intensity = 1.0f;
+	public float minIntensity = 0;
+	public float duration =  5;
+	
+	public Light light;
+	
+	float phi;
+	float amplitude;
+
+	// Use this for initialization
+	void Start () {
+		intensity = light.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		phi = Time.time/duration*2*Mathf.PI;
+		amplitude = Mathf.Cos(phi) * 0.5f + 1.5f;
+		light.intensity = amplitude;
+		//if(light.intensity>0)
+		//	light.intensity -=0.01f;
+	}
+}
